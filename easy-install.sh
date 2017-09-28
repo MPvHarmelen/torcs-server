e568bfcf3a7b75a9d9d26097930c6bbfbb459c6e
Martin van Harmelen <Martin@vanharmelen.com>
2017-09-28 12:55:06 +0200

    Tried to fix ./easy-install.sh

--- easy-install.sh
+++ easy-install.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
-xargs sudo apt-get install < system_requirements.txt
+
+# Needed for this script
+sudo apt-get install -y build-essential unzip
+
+# Needed for TORCS
+xargs sudo apt-get install -y < system_requirements.txt
 
 unzip torcs-1.3.7-patched.zip
 
-cd torcs-1.3.7-patched.zip
+cd torcs-1.3.7-patched
 
 ./configure
 make
