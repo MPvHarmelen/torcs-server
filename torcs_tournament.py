d20d822848164d62bbd48e5e748d06545f653645
Martin van Harmelen <Martin@vanharmelen.com>
2017-10-08 16:45:50 +0545

    Fixed `Player.__repr__`

--- torcs_tournament.py
+++ torcs_tournament.py
@@ -67,10 +67,20 @@ class Player(object):
         else:
             self.init_rating()
 
-    def __str__(self):
+    # def __str__(self):
+    #     return self.__class__.__name__ + "({self.token!r}, " \
+    #         "{self.rating!r}, " \
+    #         "{self.start_command!r}, " \
+    #         ")".format(self=self)
+
+    def __repr__(self):
         return self.__class__.__name__ + "({self.token!r}, " \
             "{self.rating!r}, " \
             "{self.start_command!r}, " \
+            "{self.working_dir!r}, " \
+            "{self.output_dir!r}, " \
+            "{self.stdout!r}, " \
+            "{self.stderr!r}, " \
             ")".format(self=self)
 
     def init_rating(self):
